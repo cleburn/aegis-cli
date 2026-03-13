47c630b0b569991b40004a4d3d660b84a24d8fd0
Cleburn Walker <cleburn.walker@gmail.com>
2026-03-13 18:36:20 -0500

    Remove orphaned memory command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 export { initCommand } from "./commands/init.js";
 export { explainCommand } from "./commands/explain.js";
 export { validateCommand } from "./commands/validate.js";
-export {
-  memoryCommand,
-  memoryDeleteCommand,
-  memoryClearCommand,
-} from "./commands/memory.js";
